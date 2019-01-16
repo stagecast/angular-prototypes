4acca6b001fc76f152cc3476f56b86bb373e8d2c
Filippo Boiani <filippo.boiani2@gmail.com>
2019-01-16 14:47:30 +0100

    Formly inception

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,15 @@ import { PageNotFoundComponent } from './static/not-found';
 import { WelcomeComponent } from './static/welcome';
 import { DynamicIframeComponent } from './dynamic-iframe/dynamic-iframe.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule} from '@ngx-formly/core';
+import { FormsComponent } from './forms/forms.component';
+import { FormlyFieldCustomInput } from './forms/custom-input.component';
 
 const appRoutes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'dynamic-test', component: DynamicIframeComponent },
+  { path: 'forms', component: FormsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
@@ -19,12 +24,23 @@ const appRoutes: Routes = [
     AppComponent,
     PageNotFoundComponent,
     WelcomeComponent,
-    DynamicIframeComponent
+    DynamicIframeComponent,
+    FormlyFieldCustomInput,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule, 
     ReactiveFormsModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+      ],
+      types: [
+        { name: 'custom', component: FormlyFieldCustomInput, wrappers: ['form-field'] },
+      ],
+    }),
+    FormlyBootstrapModule,
     RouterModule.forRoot(
       appRoutes,
       // { enableTracing: true } // <-- debugging purposes only
