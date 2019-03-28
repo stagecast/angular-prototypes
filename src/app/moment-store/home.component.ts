ba487099b1329a072e84a7230fd667d974d8874c
Filippo Boiani <filippo.boiani2@gmail.com>
2019-03-28 11:16:29 +0100

    Moment Store build fix

--- src/app/moment-store/home.component.ts
+++ src/app/moment-store/home.component.ts
@@ -3,6 +3,7 @@ import {Component, OnInit} from '@angular/core';
 // tslint:disable-next-line: component-selector
 @Component({selector: 'store-home-component', templateUrl: 'home.component.html'})
 export class MomentStoreComponent implements OnInit {
+  public sidebarCollapsed : boolean = true;
   constructor() {}
   ngOnInit() {}
 }
