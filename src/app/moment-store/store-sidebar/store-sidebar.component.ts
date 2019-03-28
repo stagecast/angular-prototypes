09bdac053c7e3c48614d8d822fe9d6e306214243
Filippo Boiani <filippo.boiani2@gmail.com>
2019-03-28 11:50:54 +0100

    Moment Store more styles

--- src/app/moment-store/store-sidebar/store-sidebar.component.ts
+++ src/app/moment-store/store-sidebar/store-sidebar.component.ts
@@ -4,7 +4,7 @@ import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({selector: 'app-store-sidebar', templateUrl: './store-sidebar.component.html', styleUrls: ['./store-sidebar.component.css']})
 export class StoreSidebarComponent implements OnInit {
-  @Output()onSearch : EventEmitter < string > = new EventEmitter('');
+  @Output()onSearch : EventEmitter < string > = new EventEmitter < string > (null);
   public searchControl : FormControl;
   constructor(public route : ActivatedRoute, private router : Router) {
     this.searchControl = new FormControl('');
